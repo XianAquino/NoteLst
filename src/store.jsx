55799e0b39110fade7041da1b20484cd2236b932
XianAquino <xian_a@yahoo.com>
2017-02-08 23:36:44 -0600

    redux persist

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer.jsx';
+import createLogger from 'redux-logger';
+import checkAuth from './checkAuth.jsx';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
-const store = createStore(rootReducer);
+let store = createStore(
+  rootReducer,
+  applyMiddleware(
+    createLogger(),
+    checkAuth
+  ),
+  autoRehydrate()
+);
+
+persistStore(store);
 
 export default store;
