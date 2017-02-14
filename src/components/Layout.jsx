bf487e606aeccb5448b0ebf8bb0983c16add2511
XianAquino <xian_a@yahoo.com>
2017-02-14 17:53:18 -0600

    transfer login logic and navbar to layout container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import * as loginActions from '../actions/loginActions.jsx';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const Layout = ({children}) => {
-  return(
-    <div>
-      <h1>Test</h1>
-      {children}
-    </div>
-  )
+import checkAuth from '../util/checkAuth';
+import Navbar from '../components/Navbar';
+
+class Layout extends Component {
+  componentWillMount() {
+    const { isLoggedIn, actions  } = this.props;
+    if( isLoggedIn === undefined ) {
+      checkAuth((isAuthenticated) => {
+        actions.updateLoginStatus({isLoggedIn: isAuthenticated})
+        if (!isAuthenticated) {
+          browserHistory.push('/login');
+        }
+      })
+    } else if ( isLoggedIn === false) {
+      browserHistory.push('/login');
+    }
+  }
+
+  render() {
+    const { children, isLoggedIn } = this.props;
+    if(isLoggedIn) {
+      return(
+        <div>
+          <Navbar/>
+          {children}
+        </div>
+      );
+    }
+    return null;
+  }
 };
 
-export default Layout;
+Layout.propTypes = {
+  isLoggedIn: React.PropTypes.bool,
+  actions: React.PropTypes.object,
+  children: React.PropTypes.node
+}
+
+const mapStateToProps = (state) => ({isLoggedIn: state.login.isLoggedIn});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(loginActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
