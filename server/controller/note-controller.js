fc7e2c29d94ce1069c90db9462ef60390a99606f
XianAquino <xian_a@yahoo.com>
2017-02-14 20:04:12 -0600

    creating notes

--- server/controller/note-controller.js
+++ server/controller/note-controller.js
@@ -1,32 +1,33 @@
-var notes = require('../models/note-model.js');
-var _ = require('underscore');
+const notes = require('../models/note-model.js');
+const _ = require('underscore');
 
 module.exports = {
-  get: function(req, res) {
-    var user = req.params.user_id;
+  get: (req, res) => {
+    const user = req.params.user_id;
     notes.getUserNotes(user,function(notes) {
       res.json(notes);
     });
   },
-  create: function(req, res) {
-    var user = req.params.user_id;
-    var params = _.extend({user_id:user},req.body);
-    notes.createNote(params);
-    res.send("Created");
+  create: (req, res) => {
+    const user = req.params.user_id;
+    const params = _.extend({user_id: user}, req.body);
+    notes.createNote(params, (note) => {
+      res.send(note)
+    });
   },
-  getNote: function(req, res) {
+  getNote: (req, res) => {
     notes.getNote(req.params.note_id,function(note) {
       res.json(note);
     });
   },
-  update: function(req, res) {
-    var noteId = req.params.note_id;
+  update: (req, res) => {
+    const noteId = req.params.note_id;
     notes.update(noteId, req.body);
     res.send('Updated');
   },
-  delete: function(req, res) {
-    var noteId = req.params.note_id;
+  delete: (req, res) => {
+    const noteId = req.params.note_id;
     notes.delete(noteId);
     res.send('Deleted');
   }
-}
+};
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import createNote from '../util/createNote';
+import { connect } from 'react-redux';
 
-const Notes = ({params}) => {
-  console.log("username",params);
-  return(
-    <div>
-      <h1>Notes</h1>
-    </div>
-  )
+class Notes extends Component {
+  constructor(props){
+    super(props);
+    this.create = this.create.bind(this);
+  }
+
+  create() {
+    const { userId } = this.props;
+    createNote(userId, (response) => {
+      console.log(response.insertId);
+    });
+  }
+
+  render() {
+    const { params, userId } = this.props;
+    console.log("username",params, userId);
+    return(
+      <div>
+        <h1>Notes</h1>
+        <button onClick={this.create}>Create Note</button>
+      </div>
+    )
+  }
 };
 
-export default Notes;
+Notes.propTypes = {
+  params: React.PropTypes.object,
+  userId: React.PropTypes.number
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userId: state.userInfo.id
+  }
+}
+
+export default connect(mapStateToProps)(Notes);
