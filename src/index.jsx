f75ab42385babf3fa5e3a303bdcedf240261cf80
XianAquino <xian_a@yahoo.com>
2017-03-02 12:26:34 -0600

    basic material-ui navbar

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <Provider store={store}>
