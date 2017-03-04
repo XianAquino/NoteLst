e36aba1bdd6930c8891558c727de7d8dada3653f
XianAquino <xian_a@yahoo.com>
2017-03-04 16:35:28 -0600

    fix messaging bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,6 @@ import { connect } from 'react-redux';
 import LogoutButton from './LogoutButton';
 import AppBar from 'material-ui/AppBar'
 import { browserHistory } from 'react-router';
-import AvNote from 'material-ui/svg-icons/av/note';
-import MessageIcon from 'material-ui/svg-icons/communication/mail-outline';
-import GroupsIcon from 'material-ui/svg-icons/social/group';
-import { Tabs, Tab } from 'material-ui/Tabs';
 
 const onClickTitle = () => {
   browserHistory.push('/');
@@ -20,8 +16,8 @@ const Navbar = ({userInfo}) => {
       title='NoteLst'
       onTitleTouchTap={onClickTitle}>
       <Link to='/messages'>
-        <i className="material-icons">message</i>
-        <span>Message</span>
+        <i className="material-icons">mail</i>
+        <span>Messages</span>
       </Link>
       <Link to={`/${userInfo.username}/notes`}>
         <i className="material-icons">note</i>
