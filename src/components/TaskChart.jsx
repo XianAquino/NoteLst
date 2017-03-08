7fb724ba26fa73fed2ab47d90341fa0d1bf15308
XianAquino <xian_a@yahoo.com>
2017-03-08 17:07:56 -0600

    change task chart color scheme

--- src/components/TaskChart.jsx
+++ src/components/TaskChart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BarChart } from 'react-d3-components';
+import * as d3 from 'd3';
+const colorScale = d3.scaleOrdinal().range(['#3F51B5', '#B0BEC5']);
 
 const TaskChart = () => {
 
@@ -14,7 +16,7 @@ const TaskChart = () => {
           {x: 'Feb 4', y: 10},
           {x: 'Feb 5', y: 4},
           {x: 'Feb 6', y: 2},
-          {x: 'Feb 7', y: 1}
+          {x: 'Feb 7', y: 5}
         ]
       },
       {
@@ -26,14 +28,16 @@ const TaskChart = () => {
           {x: 'Feb 4', y: 10},
           {x: 'Feb 5', y: 4},
           {x: 'Feb 6', y: 2},
-          {x: 'Feb 7', y: 1}
+          {x: 'Feb 7', y: 0}
         ]
       },
     ];
 
   return (
     <div className='task-chart'>
+      <h2>Weekly Timeline</h2>
       <BarChart
+        colorScale={colorScale}
         data={data}
         width={300}
         height={300}
