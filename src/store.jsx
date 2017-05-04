d03c75120c686800c84c0c3e4a864d6edec6e2a4
XianAquino <xian_a@yahoo.com>
2017-05-03 19:44:15 -0700

    import redux thunk to handle async actions

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer.jsx';
 import createLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
 //import { persistStore, autoRehydrate } from 'redux-persist';
 
 let store = createStore(
   rootReducer,
   applyMiddleware(
+    ReduxThunk,
     createLogger()
   ),
   // autoRehydrate()
